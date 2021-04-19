b8547c6f11154041b8352fd3689d4f7cbda7dc45
Blaz Vrataric <online.blaz@gmail.com>
2021-04-19 09:46:34 +0200

    day end mid-work commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log("It's working!");
+/*
+
+const content = (() => {
+
+    const content = document.querySelector('div#content');
+
+    const header = document.createElement('div');
+    header.setAttribute('id', 'header');
+    content.appendChild(header);
+
+    const h1 = document.createElement('h1');
+    h1.textContent = 'The Big Brain restaurant';
+    h1.setAttribute('id', 'pageTitle');
+    header.appendChild(h1);
+
+    const menu = document.createElement('div');
+    menu.setAttribute('id', 'menu');
+    header.appendChild(menu);
+
+    const button1 = document.createElement('div');
+    button1.textContent = 'home';
+    button1.setAttribute('id', 'home');
+    menu.appendChild(button1);
+
+    const button2 = document.createElement('div');
+    button2.textContent = 'menu';
+    button2.setAttribute('id', 'menu');
+    menu.appendChild(button2);
+
+    const button3 = document.createElement('div');
+    button3.textContent = 'contact';
+    button3.setAttribute('id', 'contact');
+    menu.appendChild(button3);
+
+    const buttonContent = document.createElement('div');
+    buttonContent.setAttribute('id', 'buttonContent');
+    content.appendChild(buttonContent);
+   
+})();
+
+*/
---
+++ src/main.js
@@ -0,0 +1,16 @@
+/*
+
+const main = (() => {
+
+    const content = document.querySelector('div#content');
+    
+    const pMain = document.createElement('');
+    pMain.textContent = 'We present to you the first restaurant focused on supplimenting brain growth. Our food is 108% organic and includes NO gluten, milk prodocuts, sharp objects, food poison, aliens, mercury or food-coloring. Check out our menu or contact us if you have any questions.';
+    pMain.setAttribute('id', 'pMain');
+    content.appendChild(pMain);
+    
+})();
+    
+export { main }
+
+*/
